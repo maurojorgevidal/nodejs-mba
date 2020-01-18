0ddbcce370b0801b6ea230c79ebcf2958c45e9fa
Mauro Jorge Vidal <maurjorge01@gmail.com>
2020-01-18 16:14:29 -0300

    Atualização

--- src/router/usuarioRouter.test.ts
+++ src/router/usuarioRouter.test.ts
@@ -5,13 +5,13 @@ import app from "../server";
 
 test("[GET] /usuarios", async () => {
   const mockusuarios = [{
-      _id: "123",
+      _id: "123456789101112131415167",
       username: "mauro",
       password: "jorge"
   }];
   mockingoose(UsuarioModel).toReturn(mockusuarios, "find");
 
   const response = await supertest(app).get("/usuarios");
-  expect(response.text).toBe(JSON.stringify(mockingoose));
+  expect(response.text).toBe(JSON.stringify(mockusuarios));
 
 })
